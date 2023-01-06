3b7f4165b854bc209f91f58e22b27b7ba3ead269
Ugo-Larsonneur <ugo.larsonneur0@gmail.com>
2023-01-06 17:05:02 +0100

    Ajout de déco, fin de l'eau, fix de spawner

--- Assets/Scripts/Player/JumpAnim.cs
+++ Assets/Scripts/Player/JumpAnim.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/**
+*/
 public class JumpAnim : StateMachineBehaviour
 {
     //OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
